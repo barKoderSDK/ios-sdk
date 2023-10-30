596e5e16cf765457adfc60f765ebbe13629c5c6e
barKoderSDK <admin@barkoder.com>
2023-10-30 13:20:10 +0100

    Hello World

---
+++ framework/BarkoderSDK.xcframework/ios-arm64_x86_64-simulator/BarkoderSDK.framework/Headers/BarkoderSDK.h
@@ -0,0 +1,17 @@
+//
+//  BarkoderView.h
+//  BarkoderView
+//
+//  Created on 26/12/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <Barkoder/iBarkoder.h>
+
+//! Project version number for BarkoderSDK.
+FOUNDATION_EXPORT double BarkoderSDKVersionNumber;
+
+//! Project version string for BarkoderSDK.
+FOUNDATION_EXPORT const unsigned char BarkoderSDKVersionString[];
+
+
